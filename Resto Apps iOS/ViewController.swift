8c4699883b5e9138b265654d21afdd028e38d81a
rasyidrw <rasyidrespati.30@gmail.com>
2020-02-03 10:31:44 +0700

    Initial Commit

---
+++ Resto Apps iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resto Apps iOS
+//
+//  Created by Rasyid Respati Wiriaatmaja on 03/02/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
